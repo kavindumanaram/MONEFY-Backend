1775d053686c16e3ec27d34ef0145190150b3d97
Kavindu Manaram <kavindu@calcey.com>
2020-01-20 21:03:36 +0530

    basic CRUD operations

---
+++ Models/PaymentDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }   //YY/MM
+        [Required]
+        [Column(TypeName = "nvarchar(3)")]
+        public int CVV { get; set; }
+    }
+}
---
+++ Models/paymentdetailsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class paymentdetailsContext : DbContext
+    {
+        public paymentdetailsContext(DbContextOptions<paymentdetailsContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetail { get; set; }
+    }
+}
